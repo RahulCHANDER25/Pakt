0abf25aa67cca469e42b3964463729285ea5f2ce
Tiphaine Bertone <tiphaine.bertone@epitech.eu>
2024-11-09 18:42:11 +0100

    fix(merge): restore change in game scene

--- scripts/game_scene.gd
+++ scripts/game_scene.gd
@@ -1,51 +1,35 @@
 extends Node
 
-const DraggableCard = preload("res://scripts/draggingCard.gd")
-const Card = preload("res://ressources/Card.gd")
+@export var card_scene: PackedScene
+
+const Card = preload("res://ressources/CardClass.gd")
 
 var cards = [
-	Card.new ("Crab", "Crabby crabby me", 10, -2, preload("res://assets/Sprites/kirby.png")),
-	Card.new ("Hog", "Bad hogz spells", 5, -3, preload("res://assets/Sprites/hedgehog.png")),
-	Card.new ("Crab", "Crabby crabby me", 10, -2, preload("res://assets/Sprites/kirby.png")),
-	Card.new ("Hog", "Bad hogz spells", 5, -3, preload("res://assets/Sprites/hedgehog.png")),
-	Card.new ("Crab", "Crabby crabby me", 10, -2, preload("res://assets/Sprites/kirby.png")),
-	Card.new ("Crab", "Crabby crabby me", 10, -2, preload("res://assets/Sprites/kirby.png")),
-	Card.new ("Hog", "Bad hogz spells", 5, -3, preload("res://assets/Sprites/hedgehog.png")),
-	Card.new ("Hog", "Bad hogz spells", 5, -3, preload("res://assets/Sprites/hedgehog.png")),
+	Card.new ("Mage Stone", "Hello, I'm a stone mage. Do you want pakt with me ?", [{"health": 10}, {"magic": -3}], [{"health": 10}, {"magic": -3}], preload("res://assets/Sprites/divinity/1.png")),
+	Card.new ("See Eyes", "...i give you some light...?", [{"health": 10}, {"magic": -3}], [{"health": 10}, {"magic": -3}], preload("res://assets/Sprites/divinity/2.png")),
+	Card.new ("Thief", "Hurry ! Pakt or not ?", [{"health": 10}, {"magic": -3}], [{"health": 10}, {"magic": -3}], preload("res://assets/Sprites/divinity/3.png")),
+	Card.new ("Muscle", "You envy me no ? pakt with me (of course) !", [{"health": 10}, {"magic": -3}], [{"health": 10}, {"magic": -3}], preload("res://assets/Sprites/divinity/4.png")),
+	Card.new ("Rabbit", "Mushr.. Mushrm.. Mushrmm... pakt ?", [{"health": 10}, {"magic": -3}], [{"health": 10}, {"magic": -3}], preload("res://assets/Sprites/divinity/5.png"))
 ]
 
-var active_card
+var active_card_sprite
 
 func _ready():
 	spawn_ennemy_card(cards[0])
 	randomize()
 
-func spawn_ennemy_card(card):
-	var new_card_instance = Card.new(card.name, card.description, card.positive_effect, card.negative_effect, card.texture)
-	var new_card_sprite = Sprite2D.new()
-	new_card_sprite.texture = new_card_instance.texture
-	
-	new_card_sprite.position = Vector2(get_window().size.x / 2, get_window().size.y / 2)
-	new_card_sprite.set_script(DraggableCard)
-	new_card_sprite.connect("disappeared", Callable(self, "_on_card_disparition"))
+func spawn_ennemy_card(card_info):
+	# Create a new instance of the Mob scene.
+	var card = card_scene.instantiate()
 
-	var area = Area2D.new()
-	area.name = "Area2D"
-	var collision_shape = CollisionShape2D.new()
-	var shape = RectangleShape2D.new()
-	shape.extents = new_card_sprite.texture.get_size() / 2
-	collision_shape.shape = shape
-	area.add_child(collision_shape)
-	new_card_sprite.add_child(area)
-
-	active_card_sprite = new_card_sprite
-	add_child(new_card_sprite)
+	card.setup_card(card_info)
+	card.position = Vector2(get_window().size.x / 2, get_window().size.y / 2)
+	card.connect("disappeared", Callable(self, "_on_card_disappeared"))
+	add_child(card)
 
 func _on_card_disappeared():
 	if cards.size() > 1:
 		cards.pop_front()
-		if active_card.pass_effects[0].has("health"):
-			print(active_card.pass_effects[0]["health"])
 		spawn_ennemy_card(cards[randi() % cards.size()])
 	else:
 		print("No more cards to spawn")
