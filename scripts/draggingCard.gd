72d9838887fafee2a220ee70136d59b4cf0328f7
Hugo <hugo.duchemin@epitech.eu>
2024-11-09 14:27:36 +0100

    feat: more stable system to randomize card spawning from an array

--- scripts/draggingCard.gd
+++ scripts/draggingCard.gd
@@ -1,12 +1,32 @@
 extends Sprite2D
 
+signal disappeared
+
 var dragging = false
 var drag_start_position = Vector2.ZERO
 var drag_threshold = 160
+var can_drag = false
 
 func _ready():
-	$Area2D.input_event.connect(_on_input_event)
+	var timer = Timer.new()
+	timer.set_one_shot(true)
+	timer.set_wait_time(0.5)
+	timer.connect("timeout", Callable(self, "_on_drag_delay_timeout"))
+	add_child(timer)
+	timer.start()
+	call_deferred("_setup_area2d")
 
+func _on_drag_delay_timeout():
+	can_drag = true
+
+func _setup_area2d():
+	var area = get_node_or_null("Area2D")
+	if area:
+		area.input_event.connect(_on_input_event)
+	else:
+		#Area not loaded yet
+		pass
+	
 func _on_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT:
@@ -31,12 +51,15 @@ func _check_drag_direction():
 		else:
 			_on_drag_right()
 	else:
-		position = drag_start_position
+		position = Vector2(get_window().size.x / 2, get_window().size.y / 2)
 
 func _on_drag_left():
 	print("Dragged left")
+	emit_signal("disappeared")
 	queue_free()
+	
 
 func _on_drag_right():
 	print("Dragged right")
+	emit_signal("disappeared")
 	queue_free()
--- scripts/game_scene.gd
+++ scripts/game_scene.gd
@@ -1,9 +1,49 @@
-extends Node2D
+extends Node
 
+const DraggableCard = preload("res://scripts/draggingCard.gd")
 const Card = preload("res://ressources/Card.gd")
 
 var cards = [
-	Card.new ("Heal", "Restore health", 10, preload("res://assets/Sprites/kirby.png")),
-	Card.new("Attack", "Deal damage", 15, preload("res://assets/Sprites/kirby.png")),
-	Card.new()
+	Card.new ("Crab", "Crabby crabby me", 10, -2, preload("res://assets/Sprites/kirby.png")),
+	Card.new ("Hog", "Bad hogz spells", 5, -3, preload("res://assets/Sprites/sound.png")),
+	Card.new ("Crab", "Crabby crabby me", 10, -2, preload("res://assets/Sprites/kirby.png")),
+	Card.new ("Hog", "Bad hogz spells", 5, -3, preload("res://assets/Sprites/sound.png")),
+	Card.new ("Crab", "Crabby crabby me", 10, -2, preload("res://assets/Sprites/kirby.png")),
+	Card.new ("Hog", "Bad hogz spells", 5, -3, preload("res://assets/Sprites/sound.png")),
+	Card.new ("Crab", "Crabby crabby me", 10, -2, preload("res://assets/Sprites/kirby.png")),
+	Card.new ("Hog", "Bad hogz spells", 5, -3, preload("res://assets/Sprites/sound.png")),
 ]
+
+var active_card_sprite
+
+func _ready():
+	spawn_ennemy_card(cards[0])
+	randomize()
+
+func spawn_ennemy_card(card):
+	var new_card_instance = Card.new(card.name, card.description, card.positive_effect, card.negative_effect, card.texture)
+	var new_card_sprite = Sprite2D.new()
+	new_card_sprite.texture = new_card_instance.texture
+	
+	new_card_sprite.position = Vector2(get_window().size.x / 2, get_window().size.y / 2)
+	new_card_sprite.set_script(DraggableCard)
+	new_card_sprite.connect("disappeared", Callable(self, "_on_card_disparition"))
+
+	var area = Area2D.new()
+	area.name = "Area2D"
+	var collision_shape = CollisionShape2D.new()
+	var shape = RectangleShape2D.new()
+	shape.extents = new_card_sprite.texture.get_size() / 2
+	collision_shape.shape = shape
+	area.add_child(collision_shape)
+	new_card_sprite.add_child(area)
+
+	active_card_sprite = new_card_sprite
+	add_child(new_card_sprite)
+
+func _on_card_disparition():
+	if cards.size() > 1:
+		cards.pop_front()
+		spawn_ennemy_card(cards[randi() % cards.size()])
+	else:
+		print("No more cards to spawn")
