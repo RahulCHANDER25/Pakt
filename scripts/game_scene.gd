ade807103dfeec1d3ea90fa5e6d710f518386097
rahul.chander <rahul.chander@epitech.eu>
2024-11-09 15:20:04 +0100

    feat: Icons assets for the game (hp, magic, ...)

--- scripts/game_scene.gd
+++ scripts/game_scene.gd
@@ -7,3 +7,12 @@ var cards = [
 	Card.new("Attack", "Deal damage", 15, preload("res://assets/Sprites/kirby.png")),
 	Card.new()
 ]
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#$HUD.show_message("Uwu !")
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
