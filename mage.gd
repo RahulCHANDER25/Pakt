80c9a3d3ac765b06bd32bafd4828deff98b08ec0
Hugo <hugo.duchemin@epitech.eu>
2024-11-09 02:00:00 +0100

    feat : added a draggable sprite script

--- mage.gd
+++ mage.gd
@@ -3,7 +3,7 @@ extends AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	play("idle man", 1)
+	play("idleMan", 1)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- menu.gd
+++ menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://game_scene.tscn")
+	get_tree().change_scene_to_file("res://gameScene.tscn")
 func _on_options_pressed() -> void:
 	get_tree().change_scene_to_file("res://options.tscn")
 func _on_quit_game_pressed() -> void:
