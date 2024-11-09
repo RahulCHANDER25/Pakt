f5861b7877a4df3a594b642e28edc9d56f660da9
Tiphaine Bertone <tiphaine.bertone@epitech.eu>
2024-11-09 17:20:34 +0100

    feat(scenes): create card scene

---
+++ scripts/card.gd
@@ -0,0 +1,88 @@
+extends Area2D
+
+signal disappeared
+
+var dragging = false
+var drag_start_position = Vector2.ZERO
+var drag_threshold = 160
+var can_drag = false
+var max_rotation = 20
+var original_scale: Vector2
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var timer = Timer.new()
+	timer.set_one_shot(true)
+	timer.set_wait_time(0.5)
+	timer.connect("timeout", Callable(self, "_on_drag_delay_timeout"))
+	add_child(timer)
+	timer.start()
+	original_scale = scale
+	call_deferred("_setup_area2d")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if dragging:
+		position = get_global_mouse_position()
+		var drag_distance = position.x - drag_start_position.x
+		var drag_percentage = clamp(abs(drag_distance / drag_threshold), 0, 1)
+		print(drag_percentage)
+		var rotation_amount = max_rotation * drag_percentage * sign(drag_distance)
+		var scale_amount = 1 - drag_percentage
+		if scale_amount < 0.5:
+			scale_amount = 0.5
+		rotation_degrees = rotation_amount
+		scale = Vector2(scale_amount, scale_amount)
+
+func setup_card(card_info) -> void:
+	$TextureRect.texture = card_info.texture
+
+func _on_drag_delay_timeout():
+	can_drag = true
+
+#func _setup_area2d():
+	#var area = get_node_or_null("Area2D")
+	#if area:
+		#area.input_event.connect(_on_input_event)
+	#else:
+		##if Area not loaded yet
+		#pass
+	
+func _on_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.pressed:
+				dragging = true
+				drag_start_position = get_global_mouse_position()
+			else:
+				dragging = false
+				_check_drag_direction()
+		
+func _check_drag_direction():
+	var drag_end_position = get_global_mouse_position()
+	var drag_distance = drag_end_position.x - drag_start_position.x
+	
+	if abs(drag_distance) > drag_threshold:
+		if drag_distance < 0:
+			_on_drag_left()
+		else:
+			_on_drag_right()
+	else:
+		position = Vector2(get_window().size.x / 2, get_window().size.y / 2)
+		rotation_degrees = 0
+		scale = original_scale
+
+func _on_drag_left():
+	print("Dragged left")
+	emit_signal("disappeared")
+	queue_free()
+	
+
+func _on_drag_right():
+	print("Dragged right")
+	emit_signal("disappeared")
+	queue_free()
+
+
+func _on_card_disappeared() -> void:
+	pass # Replace with function body.
--- scripts/game_scene.gd
+++ scripts/game_scene.gd
@@ -1,17 +1,16 @@
 extends Node
 
+@export var card_scene: PackedScene
+
 const DraggableCard = preload("res://scripts/draggingCard.gd")
 const Card = preload("res://ressources/Card.gd")
 
 var cards = [
-	Card.new ("Crab", "Crabby crabby me", 10, -2, preload("res://assets/Sprites/kirby.png")),
-	Card.new ("Hog", "Bad hogz spells", 5, -3, preload("res://assets/Sprites/hedgehog.png")),
-	Card.new ("Crab", "Crabby crabby me", 10, -2, preload("res://assets/Sprites/kirby.png")),
-	Card.new ("Hog", "Bad hogz spells", 5, -3, preload("res://assets/Sprites/hedgehog.png")),
-	Card.new ("Crab", "Crabby crabby me", 10, -2, preload("res://assets/Sprites/kirby.png")),
-	Card.new ("Crab", "Crabby crabby me", 10, -2, preload("res://assets/Sprites/kirby.png")),
-	Card.new ("Hog", "Bad hogz spells", 5, -3, preload("res://assets/Sprites/hedgehog.png")),
-	Card.new ("Hog", "Bad hogz spells", 5, -3, preload("res://assets/Sprites/hedgehog.png")),
+	Card.new ("Mage Stone", "Hello, I'm a stone mage. Do you want pakt with me ?", [{"health": 0}, {"mana": 0}], preload("res://assets/Sprites/divinity/1.png")),
+	Card.new ("See Eyes", "...i give you some light...?", [{"health": 0}, {"mana": 0}], preload("res://assets/Sprites/divinity/2.png")),
+	Card.new ("Thief", "Hurry ! Pakt or not ?", [{"health": 0}, {"mana": 0}], preload("res://assets/Sprites/divinity/3.png")),
+	Card.new ("Muscle", "You envy me no ? pakt with me (of course) !", [{"health": 0}, {"mana": 0}], preload("res://assets/Sprites/divinity/4.png")),
+	Card.new ("Rabbit", "Mushr.. Mushrm.. Mushrmm... pakt ?", [{"health": 0}, {"mana": 0}], preload("res://assets/Sprites/divinity/5.png"))
 ]
 
 var active_card_sprite
@@ -20,26 +19,34 @@ func _ready():
 	spawn_ennemy_card(cards[0])
 	randomize()
 
-func spawn_ennemy_card(card):
-	var new_card_instance = Card.new(card.name, card.description, card.positive_effect, card.negative_effect, card.texture)
-	var new_card_sprite = Sprite2D.new()
-	new_card_sprite.texture = new_card_instance.texture
+func spawn_ennemy_card(card_info):
+	# Create a new instance of the Mob scene.
+	var card = card_scene.instantiate()
 	
-	new_card_sprite.position = Vector2(get_window().size.x / 2, get_window().size.y / 2)
-	new_card_sprite.set_script(DraggableCard)
-	new_card_sprite.connect("disappeared", Callable(self, "_on_card_disparition"))
-
-	var area = Area2D.new()
-	area.name = "Area2D"
-	var collision_shape = CollisionShape2D.new()
-	var shape = RectangleShape2D.new()
-	shape.extents = new_card_sprite.texture.get_size() / 2
-	collision_shape.shape = shape
-	area.add_child(collision_shape)
-	new_card_sprite.add_child(area)
-
-	active_card_sprite = new_card_sprite
-	add_child(new_card_sprite)
+	card.setup_card(card_info)
+	card.position = Vector2(get_window().size.x / 2, get_window().size.y / 2)
+	#card. shape.extents = new_card_sprite.texture.get_size() / 2
+	#collision_shape.shape = shape
+	card.connect("disappeared", Callable(self, "_on_card_disappeared"))
+	add_child(card)
+	
+	#var new_card_instance = Card.new(card.name, card.description, card.positive_effect, card.negative_effect, card.texture)
+	#var new_card_sprite = Sprite2D.new()
+	#new_card_sprite.texture = new_card_instance.texture
+	#
+	#new_card_sprite.set_script(DraggableCard)
+#
+	#var area = Area2D.new()
+	#area.name = "Area2D"
+	#var collision_shape = CollisionShape2D.new()
+	#var shape = RectangleShape2D.new()
+	#shape.extents = new_card_sprite.texture.get_size() / 2
+	#collision_shape.shape = shape
+	#area.add_child(collision_shape)
+	#new_card_sprite.add_child(area)
+#
+	#active_card_sprite = new_card_sprite
+	#add_child(new_card_sprite)
 
 func _on_card_disparition():
 	if cards.size() > 1:
