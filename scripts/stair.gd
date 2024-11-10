fc95a1c7a6804691a3cced8b207708d65b7e954b
rahul.chander <rahul.chander@epitech.eu>
2024-11-10 15:59:49 +0100

    feat: Adding box obstacle for the platformer game

--- scripts/stair.gd
+++ scripts/stair.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
 @export var sky_ball_enemy: PackedScene
-@export var flying_enemy_scene: PackedScene
+@export var box_enemy_scene: PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$SkyBallTimer.wait_time = 0.5
+	$BoxTimer.wait_time = 2
+	$BoxTimer.start()
+
+	$SkyBallTimer.wait_time = 0.75
 	$SkyBallTimer.start()
 
 
@@ -17,11 +20,20 @@ func _process(delta: float) -> void:
 func _on_sky_ball_timer_timeout() -> void:
 	var sky_ball = sky_ball_enemy.instantiate()
 
+	var view = get_viewport()
 	sky_ball.position = Vector2(
-		(get_viewport().get_camera_2d().global_position.x - get_window().size.x) + randf_range(0, get_viewport().size.y),
-		get_viewport().get_camera_2d().global_position.y - (get_window().size.y)
+		(view.get_camera_2d().global_position.x - get_window().size.x) + randf_range(view.size.x / 2, view.size.x),
+		view.get_camera_2d().global_position.y - (get_window().size.y)
 	)
 	add_child(sky_ball)
 
-func spawn_fying_enemy(flying_enemy_info):
-	pass
+
+func _on_box_timer_timeout() -> void:
+	var box = box_enemy_scene.instantiate()
+
+	var view = get_viewport()
+	box.position = Vector2(
+		(view.get_camera_2d().global_position.x - get_window().size.x) + randf_range(3 * view.size.x / 4, view.size.x),
+		view.get_camera_2d().global_position.y - (get_window().size.y)
+	)
+	add_child(box)
